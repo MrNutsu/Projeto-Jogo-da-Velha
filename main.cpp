203ab4f5cefe14730fe721062ddc023cc26fddfe
João Ravache <joaobravache@gmail.com>
2024-05-27 20:20:33 -0300

    Fiz teste com Samuel
    e isso

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 /* 24-05-2024
 Início do Projeto - Jogo da Velha
+
+27-05-2024
+Abri com o Samuel o projeto no Visual Studio Code e começamos a desenvolver o código.
+
 */
 
 #include <iostream>
@@ -9,6 +13,8 @@ using namespace std;
 
 int main(){
 
+    cout<<"Helo World!";
 
+    return 0;
 }
 
